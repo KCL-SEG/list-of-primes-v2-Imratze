dcab26596d093d9dea47abfc0055fa99b8b22399
Imran Mehdiyev <mratzemehdiyev@gmail.com>
2023-10-02 13:30:22 +0100

    Add functions to generate prime numbers

--- primes.py
+++ primes.py
@@ -1,6 +1,20 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def is_prime(num):
+    for i in range (2, num):
+        if num % i == 0:
+            return False
+    return True
+
 def primes(number_of_primes):
+    if number_of_primes == 0 or number_of_primes < 0:
+        raise ValueError
+    
     list = []
+    i = 2
+    while len(list) < number_of_primes:
+        if is_prime(i) :
+            list.append(i)
+        i = i + 1
     return list
